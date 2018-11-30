40f0cd06775b521678b7143a86f974aa71c216e5
Moriander <Ahiru72@gmail.com>
2018-12-01 06:31:56 +0700

    Parse Payloads

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+
 var server = http.createServer(function (req, res) {
 
 
@@ -12,9 +14,21 @@ var server = http.createServer(function (req, res) {
 
     var method = req.method.toLowerCase();
 
-    res.end("Hat geklappt");
+    var headers = req.headers;
 
-    console.log(method);
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data',function(data){
+        buffer += decoder.write(data);
+    });
+    
+    req.on('end',function(){
+        buffer += decoder.end();
+        res.end('Hat geklappt');
+        console.log(buffer);
+    });
+
+    console.log(headers);
 });
 server.listen(3000, function () {
     console.log("Server is running on 3000");
