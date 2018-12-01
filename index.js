9a18629bf55accf625b7c550cfea34d29f36c3ac
Moriander <Ahiru72@gmail.com>
2018-12-01 08:00:27 +0700

    Https Server added

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
 
 
+var httpServer = http.createServer(function (req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTP-server
+httpServer.listen(config.httpPort, function () {
+    console.log("HTTP-Server is running on " + config.httpPort);
+});
+
+var httpServerOptions = {'key': fs.readFileSync('./https/key.pem'),
+                        'cert': fs.readFileSync('./https/cert.pe')};
+
+var httpsServer = https.createServer(httpServerOptions, function (req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTP-server
+httpsServer.listen(config.httpsPort, function () {
+    console.log("HTTPS-Server is running on " + config.httpsPort);
+});
+
+var unifiedServer = function (req, res) {
     var parsedURL = url.parse(req.url, true);
-
     var path = parsedURL.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    
     var queryStringObject = parsedURL.query;
-
     var method = req.method.toLowerCase();
-
     var headers = req.headers;
-
     var decoder = new StringDecoder('utf-8');
     var buffer = '';
-    req.on('data',function(data){
+    req.on('data', function (data) {
         buffer += decoder.write(data);
     });
-    
-    req.on('end',function(){
+
+    req.on('end', function () {
         buffer += decoder.end();
-        
+
         // check the router
-        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        
+        var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
         var data = {
-            'trimmedPath' : trimmedPath,
-            'queryStringObject' : queryStringObject,
-            'method' : method,
-            'headers' : headers,
-            'payload' : buffer       
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
         }
-    
-        chosenHandler(data, function(statusCode, payload) {
+
+        chosenHandler(data, function (statusCode, payload) {
             // actual statusCode, otherwise default to 500
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 500;
-            payload = typeof(payload) == 'object' ? payload : {};
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 500;
+            payload = typeof (payload) == 'object' ? payload : {};
             var payloadString = JSON.stringify(payload);
 
             res.setHeader('Content-Type', 'application/json');
@@ -48,30 +66,23 @@ var server = http.createServer(function (req, res) {
             res.end(payloadString);
             console.log('Hat geklappt');
 
-        })
+        });
     });
+};
 
-
-    console.log(headers);
-});
-
-// Start the server
-server.listen(config.port, function () {
-    console.log("Server is running on "+config.port);
-});
 
 // Define handlers
 var handlers = {};
 
-handlers.hello = function(data, callback){
-    let msg = {'answer': 'World!'};
-    callback(200,msg);
+handlers.hello = function (data, callback) {
+    let msg = { 'answer': 'World!' };
+    callback(200, msg);
 }
 
-handlers.notFound = function(data, callback){
+handlers.notFound = function (data, callback) {
     callback(404);
 }
 
 var router = {
-    'hello' : handlers.hello
+    'hello': handlers.hello
 };
