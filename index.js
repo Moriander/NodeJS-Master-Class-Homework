a49d8aaa0227f43452c68202bc3d08bad345e743
Moriander <Ahiru72@gmail.com>
2018-12-01 07:01:59 +0700

    Server itself works

--- index.js
+++ index.js
@@ -24,13 +24,53 @@ var server = http.createServer(function (req, res) {
     
     req.on('end',function(){
         buffer += decoder.end();
-        res.end('Hat geklappt');
-        console.log(buffer);
+        
+        // check the router
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+        
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer       
+        }
+    
+        chosenHandler(data, function(statusCode, payload) {
+            // actual statusCode, otherwise default to 500
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 500;
+            payload = typeof(payload) == 'object' ? payload : {};
+            var payloadString = JSON.stringify(payload);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log('Hat geklappt');
+
+        })
     });
+
 
     console.log(headers);
 });
+
+// Start the server
 server.listen(3000, function () {
     console.log("Server is running on 3000");
 })
 
+// Define handlers
+var handlers = {};
+
+handlers.hello = function(data, callback){
+    let msg = {'answer': 'World!'};
+    callback(200,msg);
+}
+
+handlers.notFound = function(data, callback){
+    callback(404);
+}
+
+var router = {
+    'hello' : handlers.hello
+};
